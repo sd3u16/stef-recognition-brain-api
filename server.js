3cf80593448939c2617679fb399de6a7a86f9c86
sd3u16 <s.dimitrov1617@gmail.com>
2024-12-08 14:44:59 +0200

    fixed backend

--- server.js
+++ server.js
@@ -3,15 +3,16 @@ const bodyParser = require('body-parser'); // latest version of exressJS now com
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex')
+const { ClarifaiStub, grpc } = require('clarifai-nodejs-grpc');
 
 const db = knex({
   // Enter your own database information here based on what you created
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'aneagoie',
-    password : '',
-    database : 'smart-brain'
+    host: '127.0.0.1',
+    user: 'postgres',
+    password: 'mypassword123',
+    database: 'smart_brain'
   }
 });
 
@@ -24,6 +25,45 @@ app.use(express.json()); // latest version of exressJS now comes with Body-Parse
 // app.get('/', (req, res)=> {
 //   res.send(database.users);
 // }) 
+
+const USER_ID = '6y6ldl8ni863';
+const PAT = 'c4a0669a49554e8193b06f2415c3699a';
+const APP_ID = '02ea4cab525649219d86d7b5f497ad30';
+const WORKFLOW_ID = 'workflow-43bf74';
+
+const stub = ClarifaiStub.grpc();
+const metadata = new grpc.Metadata();
+metadata.set('authorization', 'Key ' + PAT);
+
+app.post('/clarifai-workflow', (req, res) => {
+  const { imageUrl } = req.body;
+
+  stub.PostWorkflowResults(
+    {
+      user_app_id: {
+        user_id: USER_ID,
+        app_id: APP_ID
+      },
+      workflow_id: WORKFLOW_ID,
+      inputs: [
+        { data: { image: { url: imageUrl } } }
+      ]
+    },
+    metadata,
+    (err, response) => {
+      if (err) {
+        return res.status(500).json({ error: err.message });
+      }
+
+      if (response.status.code !== 10000) {
+        return res.status(400).json({ error: 'Post workflow results failed: ' + response.status.description });
+      }
+
+      const results = response.results[0];
+      res.json(results);
+    }
+  );
+});
 
 app.post('/signin', (req, res) => {
   db.select('email', 'hash').from('login')
@@ -47,11 +87,11 @@ app.post('/signin', (req, res) => {
 app.post('/register', (req, res) => {
   const { email, name, password } = req.body;
   const hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
-      trx.insert({
-        hash: hash,
-        email: email
-      })
+  db.transaction(trx => {
+    trx.insert({
+      hash: hash,
+      email: email
+    })
       .into('login')
       .returning('email')
       .then(loginEmail => {
@@ -72,13 +112,13 @@ app.post('/register', (req, res) => {
       })
       .then(trx.commit)
       .catch(trx.rollback)
-    })
+  })
     .catch(err => res.status(400).json('unable to register'))
 })
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
-  db.select('*').from('users').where({id})
+  db.select('*').from('users').where({ id })
     .then(user => {
       if (user.length) {
         res.json(user[0])
@@ -92,18 +132,18 @@ app.get('/profile/:id', (req, res) => {
 app.put('/image', (req, res) => {
   const { id } = req.body;
   db('users').where('id', '=', id)
-  .increment('entries', 1)
-  .returning('entries')
-  .then(entries => {
-    // If you are using knex.js version 1.0.0 or higher this now returns an array of objects. Therefore, the code goes from:
-    // entries[0] --> this used to return the entries
-    // TO
-    // entries[0].entries --> this now returns the entries
-    res.json(entries[0].entries);
-  })
-  .catch(err => res.status(400).json('unable to get entries'))
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+      // If you are using knex.js version 1.0.0 or higher this now returns an array of objects. Therefore, the code goes from:
+      // entries[0] --> this used to return the entries
+      // TO
+      // entries[0].entries --> this now returns the entries
+      res.json(entries[0].entries);
+    })
+    .catch(err => res.status(400).json('unable to get entries'))
 })
 
-app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
+app.listen(3001, () => {
+  console.log('app is running on port 3001');
 })
